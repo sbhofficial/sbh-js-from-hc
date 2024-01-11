11d978ed2614fe076cc410f0024537c34a6ef43b
Subhash Gohel <59219472+sbhofficial@users.noreply.github.com>
2024-01-11 07:03:47 +0000

    Created 01 Variables with notes in basics folder

---
+++ 01Basics/01variables.js
@@ -0,0 +1,17 @@
+const userID = 12345
+let userName = "SBH"
+var userEmail = "sbhofficial@email.com"
+userAddress = "India" //bad habit to define variables - not recommend 
+
+// userID = 111111 //This is not allowed
+
+userName = "SBH Official"
+userEmail = "sbh@email.com"
+userAddress = "USA"
+
+console.table([userID, userName, userEmail, userAddress]);
+
+/*
+Prefer not to use VAR
+Because of issues in the Block Scope and Functional Scope
+*/
