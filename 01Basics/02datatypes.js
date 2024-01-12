d9e3ee249fe5157a651b1a5001795cd07db5f511
Subhash Gohel <59219472+sbhofficial@users.noreply.github.com>
2024-01-12 02:20:41 +0000

    described Data types

---
+++ 01Basics/02datatypes.js
@@ -0,0 +1,28 @@
+
+console.log( 1 + 2
+    + 3 + 4); // this is not proper code writing style, code is of but code readability is not proper, so code should be write in proper way.
+
+
+// Data Types
+
+let myName = "SBH" // string
+let age = 30 // Number
+let isMarried = true // Boolean
+ 
+
+/* permeative data types*/
+// number -> 2 to 53 for small number (i also dont understand need to check in documentation)
+// bigint -> for large numbers which can be use in stock market, facebook, twitter, instagram websites.
+// string -> string can be write in "" or '' both are ok but mostly use in "".
+// boolean -> true or false there is only 2 values Yes or No.
+// null -> it is also value, it is data type but it is also value, it is using as empty value, it can be assign to variable as null directly.
+// undefined -> it is considered when we create/declare variable but not assign any value to it.
+// symbole -> it is very unique, rarely use.
+
+
+// object -> collection of data
+
+console.log(typeof myName); // it will show the type of this variable
+console.log(typeof undefined); // it will show undefined
+console.log(typeof null); // it will show an object
+
