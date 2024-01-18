5361db3b912962b4700253add94b42d170d74cb4
Subhash Gohel <59219472+sbhofficial@users.noreply.github.com>
2024-01-18 02:19:01 +0000

    learn Stach memory and Heap memory in dataTypesSummary js

--- 01Basics/dataTypesSummary.js
+++ 01Basics/dataTypesSummary.js
@@ -33,3 +33,30 @@ let newFunction = function() {  // it will return typeof function
     console.log('This is Function');
 }
 
+/* ====================== Memory in JavaScript ====================== */
+
+// Stack memory (Primitive use Stack memory), Heap memory (used Non Primitive)
+
+let userOfficeEmail = 'sbhofficial@gmail.com' // it will store in Stack memory
+let anotherEmail = userOfficeEmail // it will add copy of userOfficEmail to anotherEmail
+anotherEmail = 'useremail@email.com' // store new value in anotherEmail
+
+console.log(userOfficeEmail); // in variable it is give the copy data of previous variable not giving original data
+console.log(anotherEmail); // in variable it is give the copy data of previous variable not giving original data
+
+// NOTE : In above variables it is give the copy data of previous variable not giving original data, thats why both values are different
+
+
+let userData1 = {
+    emailAddress : 'userEmail@email.com',
+    userUPI : 'userUPI@upi'
+} // This object data will store in Heap memory, this userData variable store in Stack memory and it object will store in heap memory userData will give reference from Heap memory
+
+let userData2 = userData1 // in this object it will give original reference/value
+
+userData2.email = 'sbhOfficial@email.com'
+
+console.log(userData1.email); // it will update latest record in both object
+console.log(userData2.email); // it will update latest record in both object
+
+// NOTE : In above objectss it is give the reference/original data of previous object, thats why both values are same
